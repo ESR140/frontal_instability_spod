a02a6494a7b7289d144b0e6c3587ccf2e9561ccc
ESR <eshwar.s.ramanathan@gmail.com>
2024-08-25 07:57:53 +0530

    hide paths

--- run_KEspectra.sh
+++ run_KEspectra.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Check if the required environment variables are set
+if [ -z "$INPUT_PATH" ] || [ -z "$BASE_OUTPUT_PATH" ]; then
+    echo "Error: INPUT_PATH and BASE_OUTPUT_PATH must be set in config.sh."
+    exit 1
+fi
+
+# Use the environment variables
+input_path="${INPUT_PATH}"
+output_dir="${BASE_OUTPUT_PATH}"
+
 # Array of plane indices you want to process
 indices=(83 85 87 89 91 93 95 97 99)  # Add or modify indices as needed
-
-# Input file path
-input_file="/media/user/Extreme SSD/SIMULATION_DATA/XYZ_SS.nc"
-
-# Base output directory
-output_dir="/media/user/Extreme SSD/SIMULATION_DATA/KEresults"
 
 # Loop through each index
 for index in "${indices[@]}"
